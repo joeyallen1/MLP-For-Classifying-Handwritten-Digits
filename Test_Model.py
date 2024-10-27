43cca2e0b5321ea60a4fb8d5484398f038293523
Joey Allen <joeyallen212@gmail.com>
2024-10-27 18:23:31 -0400

    Imported mnist dataset

--- Test_Model.py
+++ Test_Model.py
@@ -1,3 +1,12 @@
 import Sequential
 import Modules
 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
