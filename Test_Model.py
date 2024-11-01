3de960d6a990f81b888dc29cd24f700217329e63
Joey Allen <joeyallen212@gmail.com>
2024-11-01 19:16:41 -0400

    Made small changes/improvements, model still maxing out at ~70% test accuracy

--- Test_Model.py
+++ Test_Model.py
@@ -12,8 +12,8 @@ x_test = x_test / 255.0
 
 x_train = x_train.reshape(-1, 28 * 28).T
 x_test = x_test.reshape(-1, 28 * 28).T
-y_train = y_train
-y_test = y_test
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
 
 print(x_train.shape)
 print(y_train.shape)
@@ -21,8 +21,14 @@ print(x_test.shape)
 print(y_test.shape)
 
 
-modules = [Linear(784, 10), ReLu(), SoftMax()]
+# modules = [Linear(784, 784), ReLu(), 
+#            Linear(784, 256), ReLu(), 
+#            Linear(256, 128), ReLu(),
+#            Linear(128, 10), SoftMax()]
+
+modules = [Linear(784, 28), ReLu(),
+           Linear(28,10), SoftMax()]
 loss = NLL()
 model = Sequential(modules, loss, x_train, x_test, y_train, y_test)
 
-model.sgd()
+model.sgd(iterations=500, lrate=0.05, every=10)
