d4fff789735c893a7d20d1bd198a086918e70563
joeyallen1 <allen.jos@northeastern.edu>
2025-03-21 00:06:16 -0400

    Added training file that uses kaggle mnist data

---
+++ train.py
@@ -0,0 +1,34 @@
+from Sequential import *
+from Modules import *
+import csv
+import pandas as pd
+
+train_df = pd.read_csv('mnist_train.csv')
+x_train = train_df.iloc[:, 1:].to_numpy()
+x_train = x_train.T
+y_train = train_df.iloc[:, 0].to_numpy()
+
+test_df = pd.read_csv('mnist_train.csv')
+x_test = test_df.iloc[:, 1:].to_numpy()
+x_test = x_test.T
+y_test = test_df.iloc[:, 0].to_numpy()
+
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+
+
+x_train = x_train / 255
+x_test = x_test / 255
+
+print(f'Training data shape: {x_train.shape}')
+print(f'Testing data shape: {x_test.shape}')
+print(f'Training data labels shape: {y_train.shape}')
+print(f'Testing data labels shape: {y_test.shape}')
+
+
+modules = [Linear(784, 28), ReLu(),
+           Linear(28,10), SoftMax()]
+loss = NLL()
+model = Sequential(modules, loss, x_train, x_test, y_train, y_test)
+
+model.sgd(iterations=500, lrate=0.05, every=10)
